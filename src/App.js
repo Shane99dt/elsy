a8d0363cdede82db95ed6722b5af4e767093d1a9
Shane99dt <dushen1999thilaksha@gmail.com>
2022-07-18 11:01:59 +0200

    some alignments

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
     super()
 
     this.state = {
-      water : 0,
+      water : 1.5,
       heart : 120,
       temperature: -10,
       steps : 3000
@@ -23,39 +23,51 @@ class App extends React.Component {
   }
 
   onHeartChange = (e) => {
-    this.setState({heart: e.target.value})
-    this.calculateWater()
+    this.setState({heart: Number(e.target.value)}, () => {
+      this.calculateWater()
+    })
+    // this.shakeImg()
   }
   onTempChange = (e) => {
-    this.setState({temperature: e.target.value})
-    this.calculateWater()
+    this.setState({temperature: Number(e.target.value)}, () => {
+      this.calculateWater()
+    })
+    // this.shakeImg()
   }
   onStepChange = (e) => {
-    this.setState({steps: e.target.value})
-    this.calculateWater()
+    this.setState({steps: Number(e.target.value)}, () => {
+      this.calculateWater()
+    })
+    // this.shakeImg()
   }
 
+  // shakeImg = () => {
+
+  // }
 
   calculateWater = () => {
     let calWater = 1.5
     if(this.state.temperature > 20){
       calWater += (this.state.temperature-20)*0.02
-      this.setState({water: calWater.toFixed(2)})
+      // this.setState({water: calWater.toFixed(2)})
     }
     if(this.state.heart > 120){
       calWater += (this.state.heart-120)*0.0008
-      this.setState({water: calWater.toFixed(2)})
+      // this.setState({water: calWater.toFixed(2)})
     }
     if(this.state.steps > 10000){
       calWater += (this.state.steps-120)*0.00002
-      this.setState({water: calWater.toFixed(2)})
+      // this.setState({water: calWater.toFixed(2)})
     }
+    this.setState({water: calWater.toFixed(2)})
+
   }
 
   render() {
     return (
       <div className="container-fluid">
         {/* <h1 className="text-primary">Hello !</h1> */}
+
         <div className="row">
 
           {/* water */}
@@ -64,6 +76,7 @@ class App extends React.Component {
             color="#3A85FF"
             value={this.state.water}
             unit="L"
+            imgClasses='material-icons shake'
           />
 
           {/* Steps */}
@@ -72,6 +85,7 @@ class App extends React.Component {
             color="black"
             value={this.state.steps}
             unit="steps"
+            imgClasses='material-icons'
             minRange={stepsMin}
             maxRange={stepsMax}
             getRange={this.onStepChange}
@@ -81,6 +95,7 @@ class App extends React.Component {
           <Box
             icon="favorite"
             color="red"
+            imgClasses='material-icons'
             value={this.state.heart}
             unit="bpm"
             minRange={heartMin}
@@ -96,6 +111,7 @@ class App extends React.Component {
             color="yellow"
             value={this.state.temperature}
             unit="°C"
+            imgClasses='material-icons'
             minRange={tempMin}
             maxRange={tempMax}
             getRange={this.onTempChange}
--- src/components/Box.js
+++ src/components/Box.js
@@ -3,10 +3,10 @@ import React from "react"
 class Box extends React.Component {
   render() {
     const isWater = this.props.icon !== "local_drink"
-    const stepsCheck = this.props.icon === "direction_walk"
+    // const stepsCheck = this.props.icon === "direction_walk"
     return (
       <div className="box col-sm-3 col-6">
-        <span style={{ color: this.props.color, fontSize:100}}className="material-icons">{this.props.icon}</span>
+        <span style={{ color: this.props.color, fontSize:100}} className={this.props.imgClasses}>{this.props.icon}</span>
         <p>
           {this.props.value}{this.props.unit}
         </p>
@@ -14,26 +14,27 @@ class Box extends React.Component {
           <input
             id={this.props.rangeId}
             type='range'
+            className="rangeInput"
             name={this.props.rangeName}
             min={this.props.minRange}
             max={this.props.maxRange}
             value={this.props.value}
-            step="1"
-            onChange={this.props.getRange}>
-          </input>
+            onChange={this.props.getRange}
+            step={this.props.icon === "directions_walk" ? 1000 : 1}>
+            </input>
         }
-        {stepsCheck &&
+        {/* {stepsCheck &&
           <input
             id={this.props.rangeId}
             type='range'
+            className="rangeInput"
             name={this.props.rangeName}
             min={this.props.minRange}
             max={this.props.maxRange}
             value={this.props.value}
-            step="1000"
             onChange={this.props.getRange}>
           </input>
-        }
+        } */}
       </div>
     )
   }
